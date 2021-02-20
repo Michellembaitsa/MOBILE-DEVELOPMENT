5ae728681619d583ca9c83a78884cd36fddc2284
Michellembaitsa <michellembaitsaa@gmail.com>
2021-02-20 23:27:18 +0300

    Assignment1

---
+++ src/Assignment1.kt
@@ -0,0 +1,9 @@
+fun main() {
+var student= arrayOf("michelleMbaitsa","0709876543",80.765432,24,"true")
+    var name="michelleMbaitsa"
+    var phone="0709876543"
+    var weight=80.765432
+    var age=24
+    var citizen="true"
+    print(student)
+}
